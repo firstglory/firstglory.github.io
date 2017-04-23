a4e6c74ebc5a515f201fd2087d27718a26a5d906
pennzht <penn.zht@gmail.com>
2017-04-23 15:39:25 -0700

    Map and Multicolor Bitmap

--- bitmap_to_json_multicolor.py
+++ bitmap_to_json_multicolor.py
@@ -8,14 +8,26 @@ x = data.size[0]
 y = data.size[1]
 print('x =', x, 'y =', y)
 
+colorlist = [(255,255,255), (208,181,0), (0,153,0), (0,120,120), (0,130,130), (0,194,194), (255,0,0), (127,127,127), (78,255,0)]
+
+# 0 = ocean
+# 1 = land
+# 2 = forest
+# 3 = mountain
+# 4 = image mountain
+# 5 = big mountain
+# 6 = volcano
+# 7 = rock
+# 8 = grass
+
 lsts = []
 for j in range(y):
 	lst = []
 	for i in range(x):
-		if data[j*x+i][0]:
-			lst.append(0)
-		else:
-			lst.append(1)
+                for k in range(len(colorlist)):
+		if (data[j*x+i][0], data[j*x+i][1], data[j*x+i][2]) == colorlist[k]:
+			lst.append(k)
+                        break
 	lsts.append(copy.deepcopy(lst))
 
 for i in range(y):
@@ -23,3 +35,4 @@ for i in range(y):
 
 with open('data.txt', 'w') as outfile:
     json.dump(lsts, outfile)
+
