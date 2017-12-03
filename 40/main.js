852244e3ba82a0182e6d8423e53ae004687ce87b
pennzht <penn.zht@gmail.com>
2017-12-03 15:17:43 -0800

    Test

--- 40/main.js
+++ 40/main.js
@@ -1,7 +1,23 @@
-console.log("LD40")
+console.log("LD40");
+
+init();
 
 function init(){
-    canvas = document.getElementById('ce')
-    c = canvas.getContext('2d')
+    canvas = document.getElementById('ce');
+    c = canvas.getContext('2d');
     c.imageSmoothingEnabled = false;
+    wall = regimg('wall');
+    setInterval(redraw, 1000);
 }
+
+function redraw(){
+    c.drawImage(wall, 10, 50);
+}
+
+function regimg(name){
+    var s = new Image();
+    s.onload = redraw;
+    s.src = 'img/' + name + '.png';
+    return s;
+}
+
