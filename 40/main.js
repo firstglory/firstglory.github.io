930261462a228f0c13b4f02af56fa4a94a4127bc
pennzht <penn.zht@gmail.com>
2017-12-03 16:18:50 -0800

    Avatar, with Turning

--- 40/main.js
+++ 40/main.js
@@ -7,8 +7,14 @@ function init(){
     c = canvas.getContext('2d');
     c.imageSmoothingEnabled = false;
     wall = regimg('wall');
-    setInterval(redraw, 1000);
+    floor = regimg('floor');
+    pleft = regimg('person_left1');
+    pright = regimg('person_right1');
+    avatar = pright;
+    setInterval(redraw, 50);
     dimensions();
+    starthearing();
+    createmap();
 }
 
 function dimensions(){
@@ -20,6 +26,35 @@ function dimensions(){
     ah = canvas.height;
     aleft = (ah - arows * asize) / 2;
     atop = aleft;
+}
+
+function createmap(){
+    map = {};
+    var disp, i, j;
+    for(i=0; i<13; i++){
+        for(j=0; j<13; j++){
+            if (i==0 || i==12 || j==0 || j==12){
+                disp = true;
+            } else if (i%2 == 0 && j%2 == 0){
+                disp = true;
+            } else if (i%2 == 1 && j%2 == 1){
+                disp = false;
+            } else {
+                disp = Math.random() < 0.5;
+            }
+            map[[i,j]]=disp;
+        }
+    }
+    loc = [1,1];
+}
+
+function cplus(a, b){
+    return [a[0]+b[0], a[1]+b[1]];
+}
+
+function pgo(b){
+    loc[0] += b[0];
+    loc[1] += b[1];
 }
 
 function redraw(){
@@ -34,21 +69,15 @@ function redraw(){
     var i, j, disp;
     for (i=0; i<arows; i++){
         for (j=0; j<acols; j++){
-            if (i==0 || i==12 || j==0 || j==12){
-                disp = true;
-            } else if (i%2 == 0 && j%2 == 0){
-                disp = true;
-            } else if (i%2 == 1 && j%2 == 1){
-                disp = false;
-            } else {
-                disp = Math.random() < 0.5;
-            }
-            if (disp){
+            if (map[[i,j]]){
                 c.drawImage(wall, aleft + asize * i, atop + asize * j);
+            }else{
+                c.drawImage(floor, aleft + asize * i, atop + asize * j);
             }
         }
     }
     c.restore(); // restore
+    c.drawImage(avatar, aleft + asize * loc[0], atop + asize * loc[1]);
 }
 
 function regimg(name){
@@ -57,4 +86,33 @@ function regimg(name){
     s.src = 'img/' + name + '.png';
     return s;
 }
+
+function starthearing(){
+    document.onkeydown = keylistener;
+}
+
+function keylistener(e){
+    var locnew;
+    switch(e.code){
+    case 'ArrowRight': case 'KeyD':
+        locnew = cplus(loc, [1, 0]);
+        avatar = pright;
+        break;
+    case 'ArrowLeft': case 'KeyA':
+        locnew = cplus(loc, [-1, 0]);
+        avatar = pleft;
+        break;
+    case 'ArrowUp': case 'KeyW':
+        locnew = cplus(loc, [0, -1]); break;
+    case 'ArrowDown': case 'KeyS':
+        locnew = cplus(loc, [0, 1]); break;
+    default:
+        locnew = loc;
+    }
+    if (! map[locnew]){
+        loc = locnew;
+    }
+    redraw();
+}
+
 
